8c50652d35e5d98c2ff31ada11ebc5bd274db457
YuukiWada <yuphys.expressif@gmail.com>
2024-02-15 20:23:44 +0900

    GPS for chrony

--- scripts/python/gps_verification.py
+++ scripts/python/gps_verification.py
@@ -8,14 +8,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.OUT)
 
 while True:
-    result = subprocess.run("ntpq -p", shell=True, stdout=subprocess.PIPE , stderr=subprocess.PIPE ,encoding="utf-8")
+    result = subprocess.run("chronyc sources -v", shell=True, stdout=subprocess.PIPE , stderr=subprocess.PIPE ,encoding="utf-8")
     lines = result.stdout.split("\n")
-    print(lines[2].split())
-    reach = int(lines[2].split()[6])
-
-    if reach>=17:
-        GPIO.output(27,1) 
-    else:
-        GPIO.output(27,0) 
-    time.sleep(10)
-
+    for line in lines:
+        parse = line.split()
+        if len(parse)>1:
+            if parse[1] == "GPS":
+                if parse[0] == "#*":
+                    print("GPS synchronized")
+                    GPIO.output(27,1)
+                else:
+                    print("GPS not synchronized")
+                    GPIO.output(27,0)
+    time.sleep(30)
